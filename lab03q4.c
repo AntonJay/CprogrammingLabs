229a5ea95bcd2cac6d0d5f4f09be747266c68ced
AntonJay <135606326+AntonJay@users.noreply.github.com>
2023-06-05 16:06:56 +0530

    did the lab 03 question 05

---
+++ lab03q4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n1, n2;
+
+    printf("Enter two integers: ");
+    scanf("%d %d", &n1, &n2);
+
+    if (n1 % n2 == 0) {
+        printf("%d is a multiple of %d.\n", n1, n2);
+    } else {
+        printf("%d is not a multiple of %d.\n", n1, n2);
+    }
+
+    return 0;
+}
